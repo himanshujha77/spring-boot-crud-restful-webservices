767c2d0f146790d338320a59d93dd7954ef926b1
Himanshu Jha <himanshu.jha.777@gmail.com>
2025-09-25 14:10:33 +0530

    initial commit

---
+++ src/main/java/org/him/spring_boot_crud_restful_webservices/SpringBootCrudRestfulWebservicesApplication.java
@@ -0,0 +1,13 @@
+package org.him.spring_boot_crud_restful_webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudRestfulWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudRestfulWebservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/him/spring_boot_crud_restful_webservices/SpringBootCrudRestfulWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.him.spring_boot_crud_restful_webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudRestfulWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
