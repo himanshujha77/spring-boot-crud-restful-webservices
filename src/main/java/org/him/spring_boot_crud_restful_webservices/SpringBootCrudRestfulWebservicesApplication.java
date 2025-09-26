4beded93cc5e7b63145e8192cc934710d083e80d
Himanshu Jha <himanshu.jha.777@gmail.com>
2025-09-26 15:36:26 +0530

    add default constructor for User

--- src/main/java/org/him/spring_boot_crud_restful_webservices/SpringBootCrudRestfulWebservicesApplication.java
+++ src/main/java/org/him/spring_boot_crud_restful_webservices/SpringBootCrudRestfulWebservicesApplication.java
@@ -3,11 +3,12 @@ package org.him.spring_boot_crud_restful_webservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication  // Combines @Configuration, @EnableAutoConfiguration, and @ComponentScan
 public class SpringBootCrudRestfulWebservicesApplication {
 
+	// Main method: entry point of the Spring Boot application
 	public static void main(String[] args) {
+		// Bootstraps the application, starts Spring context and embedded server
 		SpringApplication.run(SpringBootCrudRestfulWebservicesApplication.class, args);
 	}
-
 }
--- src/main/java/org/him/spring_boot_crud_restful_webservices/entity/User.java
+++ src/main/java/org/him/spring_boot_crud_restful_webservices/entity/User.java
@@ -19,6 +19,8 @@ public class User {
     @Column(name = "email")
     private String email;
 
+    public User(){}
+
     public User(String firstName, String lastName, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
